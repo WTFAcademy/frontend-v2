cd141ba1710edfdc219943e1c4a1afc397da8a4b
GlacierLuo <1090490148@qq.com>
2024-12-12 22:08:00 +0800

    fix: Fix type issues.

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -1,8 +1,11 @@
 import 'server-only'
- 
+
+type Locale = 'en' | 'zh'
+
 const dictionaries = {
   en: () => import('@/public/dictionaries/en.json').then((module) => module.default),
   zh: () => import('@/public/dictionaries/zh.json').then((module) => module.default),
 }
- 
-export const getDictionary = async (locale) => dictionaries[['zh', 'en'].includes(locale) ? locale : 'zh']()
+
+export const getDictionary = async (locale: string | null = 'zh') => 
+  dictionaries[(['zh', 'en'].includes(locale ?? 'zh') ? locale : 'zh') as Locale]()
