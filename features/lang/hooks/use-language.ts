4b5abd8d27d91d5681f2a42e4dec33a5d107118f
daxiongya <chongqiangchen@gmail.com>
2025-01-05 18:52:57 +0800

    fix: 修正多语言逻辑

--- features/lang/hooks/use-language.ts
+++ features/lang/hooks/use-language.ts
@@ -6,16 +6,16 @@ export const useLanguage = () => {
   const currentLang = pathname?.split('/')[1] || 'zh';
 
   const changeLanguage = (locale: 'zh' | 'en') => {
-    // 2. 设置 cookie
+    // 1. 设置 cookie
     document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=31536000`
     
-    // 3. 构建新的 URL 路径
+    // 2. 构建新的 URL 路径
     const newPathname = pathname.replace(`/${currentLang}`, `/${locale}`)
     
-    // 4. 使用 router.replace 而不是 push 来避免在历史记录中添加新条目
+    // 3. 使用 router.replace 而不是 push 来避免在历史记录中添加新条目
     router.replace(newPathname)
     
-    // 5. 强制刷新页面以清除缓存数据
+    // 4. 强制刷新页面以清除缓存数据
     router.refresh()
   }
 
