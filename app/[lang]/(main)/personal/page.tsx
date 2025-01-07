fc8272ea00f7db56e861a4a62c397b41936f37b1
daxiongya <chongqiangchen@gmail.com>
2025-01-07 13:52:31 +0800

    fix: 修正个人中心

--- app/[lang]/(main)/personal/page.tsx
+++ app/[lang]/(main)/personal/page.tsx
@@ -30,24 +30,16 @@ const PersonalPage = () => {
   return (
     <>
       <div id="personal" className="relative min-h-screen flex flex-col">
-        <div className="absolute w-full h-[519px] inset-x-0 top-0 md:block">
-          <Image
-            src="/images/personal-banner.png"
-            alt="Personal Center Hero"
-            fill
-            className="absolute w-full h-full object-cover"
-          />
-          <div className="absolute w-full h-full inset-0 bg-gradient-to-b from-[rgba(255,255,255,0)] from-[-26.57%] via-[rgba(255,255,255,0)] via-0% to-white to-100% dark:from-[rgba(18,18,18,0)] dark:via-[rgba(18,18,18,0)]  dark:to-[rgba(18,18,18,1)]"></div>
-        </div>
+        <div id="personal-banner" className="absolute w-full h-[761px] md:h-[539px] inset-x-0 top-0 md:block opacity-60" />
         <section className="container relative z-10 container w-full flex flex-col">
-          <div className="flex flex-col items-center px-10 pb-10">
+          <div className="flex flex-col items-center px-4 md:px-10 pb-10">
             <div className="p-[5px] bg-white rounded-full w-fit mt-[135px]">
               <UserAvatar size={128} className="w-[128px] h-[128px]" />
             </div>
             <div className="mt-5 mb-6 text-[32px] font-bold">
               {authUser?.nickname || authUser?.username}
             </div>
-            <div className="flex flex-col md:flex-row bg-wtf-background-navbar border border-wtf-background-navbar2 rounded-2xl w-full min-h-[50px] p-8 gap-4">
+            <div className="flex flex-col md:flex-row bg-wtf-background-navbar border-[0.5px] border-wtf-background-navbar2 backdrop-blur-[40px] rounded-2xl w-full min-h-[50px] p-8 gap-4">
               <div className="flex-1 flex flex-col gap-4">
                 <p className="text-wtf-content-3">{authUser?.bio || t.personal.bio}</p>
                 <div className="flex flex-col md:flex-row gap-4">
--- app/[lang]/(main)/settings/page.tsx
+++ app/[lang]/(main)/settings/page.tsx
@@ -13,7 +13,11 @@ import { Form, FormField } from "@/components/ui/form";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { updateUser, unbindWallet } from "@/features/user/api/use-user-api";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { useSetAtom } from "jotai";
 import { openAuthModal } from "@/features/auth/atoms/auth";
 import { useDictionary } from "@/features/lang";
@@ -24,70 +28,67 @@ import { ChevronLeft } from "lucide-react";
 const PersonalPage = () => {
   const router = useRouter();
   const t = useDictionary();
-  const { authUser, refetchAuthUser, setIsRegistering } = useAuth()
+  const { authUser, refetchAuthUser, setIsRegistering } = useAuth();
   const setOpenLoginModal = useSetAtom(openAuthModal);
   const form = useForm({
     defaultValues: {
       nickname: authUser?.nickname || "",
       bio: authUser?.bio || "",
     },
-  })
+  });
 
   const onSubmit = async (data: any) => {
-    console.log(data)
+    console.log(data);
     try {
-      await updateUser(data)
-      refetchAuthUser()
-      toast.success(t.settings.Settings_updated_successfully)
+      await updateUser(data);
+      refetchAuthUser();
+      toast.success(t.settings.Settings_updated_successfully);
     } catch (error) {
-      console.log(error)
-      toast.error(t.settings.Failed_to_update_settings)
+      console.log(error);
+      toast.error(t.settings.Failed_to_update_settings);
     }
-  }
+  };
 
   const unbind = async () => {
     try {
-      await unbindWallet({ address: authUser?.wallet_address || "", provider: authUser?.wallet_provider || "" })
-      refetchAuthUser()
-      toast.success(t.settings.Wallet_unbound_successfully)
+      await unbindWallet({
+        address: authUser?.wallet_address || "",
+        provider: authUser?.wallet_provider || "",
+      });
+      refetchAuthUser();
+      toast.success(t.settings.Wallet_unbound_successfully);
     } catch (error) {
-      console.log(error)
-      toast.error(t.settings.Failed_to_unbind_wallet)
+      console.log(error);
+      toast.error(t.settings.Failed_to_unbind_wallet);
     }
-  }
+  };
 
   useEffect(() => {
     form.reset({
       nickname: authUser?.nickname || "",
       bio: authUser?.bio || "",
-    })
-  }, [authUser])
+    });
+  }, [authUser]);
 
   return (
     <>
       <div id="personal" className="relative min-h-screen flex flex-col px-4">
-        <div className="container relative z-10 pt-24 pl-4 md:pl-10 4xl:pl-[24rem]">
-          <Button
-            variant="outline"
-            className="flex items-center gap-1 rounded p-2"
-            onClick={() => router.back()}
-          >
-            <ChevronLeft className="h-4 w-4" />
-            <span>{t.settings.Back}</span>
-          </Button>
-        </div>
-        <div className="absolute w-full h-[251px] inset-x-0 top-0 md:block">
-          <Image
-            src="/images/personal-banner.png"
-            alt="Personal Center Hero"
-            fill
-            className="absolute w-full h-full object-cover"
-          />
-          <div className="absolute w-full h-full inset-0 bg-gradient-to-b from-[rgba(255,255,255,0)] from-[-26.57%] via-[rgba(255,255,255,0)] via-0% to-white to-100% dark:from-[rgba(18,18,18,0)] dark:via-[rgba(18,18,18,0)]  dark:to-[rgba(18,18,18,1)]"></div>
-        </div>
+        <div
+          id="personal-banner"
+          className="absolute w-full h-[276px] md:h-[251px] inset-x-0 top-0 md:block opacity-60"
+        />
         <section className="container flex-1 relative z-10 w-full flex flex-col">
-          <div className="flex flex-col items-center px-10 pb-10 4xl:pb-20">
-            <div className="mt-[116px] text-center space-y-3">
+          <div className="relative flex flex-col items-center px-10 pb-10 4xl:pb-20">
+            <Button
+              variant="outline"
+              className="flex items-center gap-1 rounded p-2 absolute top-[100px] md:top-[116px] md:left-10 left-0 border-none"
+              onClick={() => router.back()}
+            >
+              <ChevronLeft className="h-4 w-4" />
+              <span>{t.settings.Back}</span>
+            </Button>
+            <div className="md:hidden h-[33px] w-full" />
+            <div className="mt-[100px] md:mt-[116px] text-center space-y-3">
               <h1 className="text-[36px] font-bold">{t.settings.Settings}</h1>
               <p className="text-wtf-content-3 text-base">
                 {t.settings.Settings_related_to_your_account}
@@ -122,45 +123,62 @@ const PersonalPage = () => {
                     </span>
                   </div>
                 </div>
-                {
-                  authUser?.wallet_address
-                    ? (<div className="flex items-center gap-x-2">
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <Button variant="destructive">{t.settings.Unbind}</Button>
-                          </PopoverTrigger>
-                          <PopoverContent className="w-34">
-                            <Button variant="destructive" onClick={unbind}>{t.settings.Confirm}</Button>
-                          </PopoverContent>
-                        </Popover>
-                        <Button variant="secondary" size="icon" className="h-9 w-9">
-                          <Icons.copy className="w-4 h-4" onClick={() => {
-                            navigator.clipboard.writeText(authUser?.wallet_address || "")
-                            toast.success(t.settings.Copied_to_clipboard)
-                          }} />
+                {authUser?.wallet_address ? (
+                  <div className="flex items-center gap-x-2">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="destructive">
+                          {t.settings.Unbind}
                         </Button>
-                      </div>)
-                    : (
-                      <div className="flex items-center gap-x-2">
-                        <Button onClick={() => {
-                          setOpenLoginModal(true)
-                          setIsRegistering(true)
-                        }}>{t.settings.Bind_Wallet}</Button>
-                      </div>
-                    )
-                }
+                      </PopoverTrigger>
+                      <PopoverContent className="w-34">
+                        <Button variant="destructive" onClick={unbind}>
+                          {t.settings.Confirm}
+                        </Button>
+                      </PopoverContent>
+                    </Popover>
+                    <Button variant="secondary" size="icon" className="h-9 w-9">
+                      <Icons.copy
+                        className="w-4 h-4"
+                        onClick={() => {
+                          navigator.clipboard.writeText(
+                            authUser?.wallet_address || ""
+                          );
+                          toast.success(t.settings.Copied_to_clipboard);
+                        }}
+                      />
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="flex items-center gap-x-2">
+                    <Button
+                      onClick={() => {
+                        setOpenLoginModal(true);
+                        setIsRegistering(true);
+                      }}
+                    >
+                      {t.settings.Bind_Wallet}
+                    </Button>
+                  </div>
+                )}
               </div>
             </div>
 
             <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-y-10">
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col gap-y-10"
+              >
                 <FormField
                   control={form.control}
                   name="nickname"
                   render={({ field }) => (
                     <div className="flex flex-col gap-y-3">
                       <h3 className="font-bold">{t.settings.Nickname}</h3>
-                      <Input placeholder={t.settings.Enter_your_nickname} {...field} />
+                      <Input
+                        placeholder={t.settings.Enter_your_nickname}
+                        {...field}
+                      />
                     </div>
                   )}
                 />
@@ -170,17 +188,22 @@ const PersonalPage = () => {
                   render={({ field }) => (
                     <div className="flex flex-col gap-y-3">
                       <h3 className="font-bold">{t.settings.Bio}</h3>
-                      <Textarea placeholder={t.settings.Enter_your_bio} rows={4} {...field} />
+                      <Textarea
+                        placeholder={t.settings.Enter_your_bio}
+                        rows={4}
+                        {...field}
+                      />
                       <p className="text-wtf-content-3 text-sm">
                         {t.settings.Write_a_few_sentences_about_yourself}
                       </p>
                     </div>
                   )}
                 />
-                <Button className="w-fit" type="submit">{t.settings.Save}</Button>
+                <Button className="w-fit" type="submit">
+                  {t.settings.Save}
+                </Button>
               </form>
             </Form>
-
           </div>
         </section>
       </div>
